d2e719875071b6ca84eabee1a48715fb5c431115
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-06-09 08:15:55 -0700

    Practice using components.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setCart] = useState(false);
+
+  function handleAdd(){
+    setCart((inCart) => !inCart);
+  }
+
+  const cartState = inCart ? "in-cart" : "";
+  const cartText = inCart ? "Remove From Cart" : "Add to Cart";
+  const buttonClass = inCart ? "remove" : "add";
+
   return (
-    <li className="">
+    <li className={cartState}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={handleAdd}>{cartText}</button>
     </li>
   );
 }
